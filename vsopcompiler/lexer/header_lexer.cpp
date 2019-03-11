15a1d381ca16f21379dc661891f45e9f82adc5fd
JesusAlguacil <43850115+jalguacilgon@users.noreply.github.com>
2019-03-11 19:42:52 +0100

    Update header_lexer.cpp

--- vsopcompiler/lexer/header_lexer.cpp
+++ vsopcompiler/lexer/header_lexer.cpp
@@ -61,47 +61,47 @@ int returnAndDisplayToken (int token, int lineno, int charno, bool display) {
     if (display) {
         std::cout << lineno << "," << charno << ",";
         switch (token) {
-            case 259: std::cout << "end-of-file"; break;
-            case 260: std::cout << "bool"; break;
-            case 261: std::cout << "int32"; break;
-            case 262: std::cout << "string"; break;
-            case 263: std::cout << "unit"; break;
-            case 264: std::cout << "type-identifier," << *yylval.strValue; break;
-            case 267: std::cout << "integer-literal," << yylval.intValue; break;
-            case 268: std::cout << "string-literal," << *yylval.strValue; break;
-            case 269: std::cout << "and"; break;
-            case 270: std::cout << "not"; break;
-            case 271: std::cout << "class"; break;
-            case 272: std::cout << "do"; break;
-            case 273: std::cout << "else"; break;
-            case 274: std::cout << "extends"; break;
-            case 266: std::cout << "false"; break;
-            case 275: std::cout << "if"; break;
-            case 276: std::cout << "in"; break;
-            case 277: std::cout << "isnull"; break;
-            case 278: std::cout << "let"; break;
-            case 279: std::cout << "new"; break;
-            case 280: std::cout << "then"; break;
-            case 265: std::cout << "true"; break;
-            case 281: std::cout << "while"; break;
-            case 282: std::cout << "object-identifier," << *yylval.strValue; break;
-            case 283: std::cout << "lbrace"; break;
-            case 284: std::cout << "rbrace"; break;
-            case 285: std::cout << "lpar"; break;
-            case 286: std::cout << "rpar"; break;
-            case 287: std::cout << "colon"; break;
-            case 288: std::cout << "semicolon"; break;
-            case 289: std::cout << "comma"; break;
-            case 290: std::cout << "plus"; break;
-            case 291: std::cout << "minus"; break;
-            case 292: std::cout << "times"; break;
-            case 293: std::cout << "div"; break;
-            case 294: std::cout << "pow"; break;
-            case 295: std::cout << "dot"; break;
-            case 296: std::cout << "equal"; break;
-            case 297: std::cout << "lower"; break;
-            case 298: std::cout << "lower-equal"; break;
-            case 299: std::cout << "assign"; break;
+            case 259: std::cout << "end-of-file"; break;    //END token
+            case 260: std::cout << "bool"; break;           //BOOL token
+            case 261: std::cout << "int32"; break;          //INT32 token
+            case 262: std::cout << "string"; break;         //STRING token
+            case 263: std::cout << "unit"; break;           //UNIT token
+            case 264: std::cout << "type-identifier," << *yylval.strValue; break;   //TYPEID token
+            case 267: std::cout << "integer-literal," << yylval.intValue; break;    //INTLITERAL token
+            case 268: std::cout << "string-literal," << *yylval.strValue; break;    //STRLITERAL token
+            case 269: std::cout << "and"; break;            //AND token
+            case 270: std::cout << "not"; break;            //NOT token
+            case 271: std::cout << "class"; break;          //CLASS token
+            case 272: std::cout << "do"; break;             //DO token
+            case 273: std::cout << "else"; break;           //ELSE token
+            case 274: std::cout << "extends"; break;        //EXTENDS token
+            case 266: std::cout << "false"; break;          //FALSE token
+            case 275: std::cout << "if"; break;             //IF token
+            case 276: std::cout << "in"; break;             //IN token
+            case 277: std::cout << "isnull"; break;         //ISNULL token
+            case 278: std::cout << "let"; break;            //LET token
+            case 279: std::cout << "new"; break;            //NEW token
+            case 280: std::cout << "then"; break;           //THEN token
+            case 265: std::cout << "true"; break;           //TRUE token
+            case 281: std::cout << "while"; break;          //WHILE token
+            case 282: std::cout << "object-identifier," << *yylval.strValue; break; //OBJECTID token
+            case 283: std::cout << "lbrace"; break;         //LBRACE token
+            case 284: std::cout << "rbrace"; break;         //RBRACE token
+            case 285: std::cout << "lpar"; break;           //LPAR token
+            case 286: std::cout << "rpar"; break;           //RPAR token
+            case 287: std::cout << "colon"; break;          //COLON token
+            case 288: std::cout << "semicolon"; break;      //SEMICOLON token
+            case 289: std::cout << "comma"; break;          //COMMA token
+            case 290: std::cout << "plus"; break;           //PLUS token
+            case 291: std::cout << "minus"; break;          //MINUS token
+            case 292: std::cout << "times"; break;          //TIMES token
+            case 293: std::cout << "div"; break;            //DIV token
+            case 294: std::cout << "pow"; break;            //POW token
+            case 295: std::cout << "dot"; break;            //DOT token
+            case 296: std::cout << "equal"; break;          //EQUAL token
+            case 297: std::cout << "lower"; break;          //LOWER token
+            case 298: std::cout << "lower-equal"; break;    //LOWER-EQUAL token
+            case 299: std::cout << "assign"; break;         //ASSIGN token
         }
         std::cout << std::endl;
     }
